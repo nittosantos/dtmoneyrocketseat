10819400c7f0ca337ddeae3cb4e62752c2c1206c
wellyngtonTC <wellyngton.santos@externo.tc.com.br>
2021-11-16 22:52:43 -0300

    adicionando o estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,10 @@
-export const App = () => <h1>Hello World</h1>;
+import { GlobalStyle } from './styles/global';
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </>
+  );
+};
---
+++ src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+:root{
+    --background: #f0f2f5;
+    --red: #E52e54;
+    --purple: #5429CC;
+    --green: #33CC95;
+    --purple-light: #6933FF;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --shape: #FFFFFF
+}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`;
